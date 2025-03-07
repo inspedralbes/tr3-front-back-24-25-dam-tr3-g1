f99ee4daa0b9829b62b7c2d4474c77273284a127
Eric Gómez Vilà <105845066+EGVPedralbes@users.noreply.github.com>
2025-03-07 10:51:43 +0100

    Change army manager

--- vue/vuetify-project/src/pages/ArmyManager.vue
+++ vue/vuetify-project/src/pages/ArmyManager.vue
@@ -1,45 +1,73 @@
 <template>
-    <h1>Army Manager</h1>
-    
+    <h1>Gestor d'Exèrcit</h1>
+
     <div class="scroll-picker" style="display: flex; justify-content: center;">
-        <div style="display: grid; grid-template-columns: repeat(4, 1fr); gap: 10px;">
-            <div v-for="unit in 4" :key="unit" style="display: flex; flex-direction: column; align-items: center;">
-            <div v-for="(option, index) in options" :key="option.value" class="picker-item" style="display: flex; flex-direction: column; align-items: center; margin: 0 10px;">
-                <label :for="'option-'+ unit +'-'+ index"><img :src="option.icon" :alt="option.label" class="unit-image" :class="{ 'selected': army[unit-1] === option.value }" style="width: 100px; height: 100px;" /></label>
-                <input type="radio" :value="option.value" v-model="army[unit-1]" :id="'option-'+ unit +'-'+ index"/>
-            </div>
-            </div>
-        </div>
+        <v-card>
+            <v-card-title>
+                <h2>Unitats</h2>
+            </v-card-title>
+            <v-card-text>
+                <v-btn @click="createUnit()">Nova unitat</v-btn>
+            <v-simple-table>
+                <thead>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Imatge</th>
+                        <th>Arma</th>
+                        <th>Alat</th>
+                        <th>Salut</th>
+                        <th>Atac</th>
+                        <th>Moviment</th>
+                        <th>Resistència a Espases</th>
+                        <th>Resistència a Destral</th>
+                        <th>Resistència a Llances</th>
+                        <th>Resistència a Arcs</th>
+                        <th>Resistència a Màgia</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="unit in units" :key="unit.ID">
+                        <td>{{ unit.NAME }}</td>
+                        <td><img :src="unit.imatge" alt="imatge unitat" style="width: 50px; height: 50px;"></td>
+                        <td>{{ unit.WEAPON }}</td>
+                        <td>{{ unit.WINGED ? 'Sí' : 'No' }}</td>
+                        <td>{{ unit.HEALTH }}</td>
+                        <td>{{ unit.ATK }}</td>
+                        <td>{{ unit.MOVEMENT }}</td>
+                        <td>{{ unit.SWORD }}</td>
+                        <td>{{ unit.AXE }}</td>
+                        <td>{{ unit.SPEAR }}</td>
+                        <td>{{ unit.BOW }}</td>
+                        <td>{{ unit.MAGIC }}</td>
+                        <td><v-btn @click="editUnit(unit.ID)">Editar</v-btn></td>
+                        <td><v-btn @click="deleteUnit(unit.ID)">Eliminar</v-btn></td>
+                    </tr>
+                </tbody>
+            </v-simple-table>
+            </v-card-text>
+        </v-card>
+
     </div>
 </template>
 <script setup>
-import { ref } from 'vue'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
 
-const army = ref([])
-import imgH from '@/assets/Halberd.png'
-import imgSw from '@/assets/Sword.png'
-import imgA from '@/assets/archer.png'
-import imgSp from '@/assets/Spear.png'
-
-let options = [
-    { value: 'h', label: 'Halberd', icon: imgH },
-    { value: 'sw', label: 'Sword', icon: imgSw },
-    { value: 'a', label: 'Archer', icon: imgA },
-    { value: 'sp', label: 'Spear', icon: imgSp }
-
-]
+const router = useRouter()
+const units = ref([])
 onMounted(() => {
-    console.log('mounted')
-    army.value = ['h', 'sw', 'a', 'sp']
+    if (!appStore.state.user) {
+        router.push('/login')
+    }
+    fetch('http://localhost:4000/units')
+        .then(response => response.json())
+        .then(data => {
+            units.value = data
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
 })
 </script>
-<style scoped>
-.selected {
-    border: 3px solid red;
-    border-radius: 5px;
-}
-input[type="radio"] {
-    display: none;
-}
-</style>
+<style scoped></style>
