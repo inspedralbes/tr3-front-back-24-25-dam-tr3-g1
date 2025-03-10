c1e2006414007767c29684df347d53ef6775d656
Climent Fernández <a23cliferand@inspedralbes.cat>
2025-03-10 12:29:16 +0100

    Update database host configuration, change Docker port, and enhance Vue layout with navigation tabs

--- vue/vuetify-project/src/layouts/default.vue
+++ vue/vuetify-project/src/layouts/default.vue
@@ -6,9 +6,11 @@
     <v-app-bar-title>Emblema de fuego destinos: conquista</v-app-bar-title>
 
     <v-tabs>
+      <v-tab to="/">Inici</v-tab>
       <v-tab>Descarrega</v-tab>
       <v-tab>Estadístiques</v-tab>
       <v-tab>Canviar exercit</v-tab>
+      <v-tab to="/login">Login</v-tab>
     </v-tabs>
   </v-app-bar>
   <v-main>
--- vue/vuetify-project/vite.config.mjs
+++ vue/vuetify-project/vite.config.mjs
@@ -64,6 +64,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    watch: {
+      usePolling: true
+    }
   },
   css: {
     preprocessorOptions: {
